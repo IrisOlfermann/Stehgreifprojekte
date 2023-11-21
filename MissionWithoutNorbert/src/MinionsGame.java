81fa39dc51101fd3c32fb801679fdc7568c4ae0b
Dominik <dominik.kulak.15@gmail.com>
2023-11-21 13:18:35 +0100

    Update MinionsGame.java
    nur was geht ab

--- MissionWithoutNorbert/src/MinionsGame.java
+++ MissionWithoutNorbert/src/MinionsGame.java
@@ -1,5 +1,5 @@
 public class MinionsGame {
   public static void main(String[] args) throws Exception {
-    System.out.println("Hello, World!");
+    System.out.println("Hello, World was geht ab!");
 }
 }
