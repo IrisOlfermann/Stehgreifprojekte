366b1d696d79220c3a15c5c245e2ab9cb2cf5cae
Iris Olfermann <iris.olfermann@aoe.com>
2023-11-21 21:30:11 +0100

    Static Scanner

--- MissionWithoutNorbert/src/MinionsGame.java
+++ MissionWithoutNorbert/src/MinionsGame.java
@@ -5,10 +5,10 @@ public class MinionsGame {
    * @param args
    */
   public static void main(String[] args) {
-    Scanner myObj = new Scanner(System.in);
+    Scanner StaticScanner = new Scanner(System.in);
     final int MINION = 10;
     int randomPosition = (int) Math.random() * 10 + 1;
-    int x = myObj.nextInt();
+    int x = StaticScanner.nextInt();
     // hallo ich bins Dominik
     int a = 0;
 
