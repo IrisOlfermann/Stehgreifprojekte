ea290744608e4544fe90714d4d84ace70e2f145e
AYAxDesign <aya.akutsu@aoe.com>
2023-11-22 09:22:26 +0100

    First setup

--- MissionWithoutNorbert/src/MinionsGame.java
+++ MissionWithoutNorbert/src/MinionsGame.java
@@ -3,14 +3,44 @@ import java.util.Scanner;
 public class MinionsGame {
   /**
    * @param args
+   * param steht für Parameter und args für Argumente.
+   * Katze miau
    */
   public static void main(String[] args) {
     Scanner StaticScanner = new Scanner(System.in);
+
     final int MINION = 10;
-    int randomPosition = (int) Math.random() * 10 + 1;
-    int x = StaticScanner.nextInt();
-    // hallo ich bins Dominik
-    int a = 0;
+    final char NORBERT = 'O';
+    final int MAX_DRAW = 3;
+
+    // Variablen um die Position zu bestimmen
+    int randomPosition = (int) (Math.random() * 10);
+    int leftSide = randomPosition;
+    int rightSide = MINION - leftSide;
+
+    // Variablen für die Teamgröße und Endausgabe?
+    int computerTeamSize;
+    int userTeamSize;
+
+    // Benutzer Optionen
+    char drawSide;
+
+    System.out.println("Mission without Nobert!");
+
+    // Minions + Nobert aufgestellt!
+    for (int i=0; i<leftSide; i++){
+      System.out.print('X'+" ");
+    }
+    System.out.print(NORBERT+" ");
+    for (int i=0; i<rightSide; i++){
+      System.out.print('X'+" ");
+    }
+    System.out.println();
+
+    System.out.println("Wählen Sie ihre stärkste Mannschaft!");
+    System.out.println("Tipp: Nobert (O) ist am schwächsten");
+    System.out.println("Es dürfen maximal 3 Minions pro Runde ziehen");
+    System.out.println("Du bist am zug. Von welcher Seite möchtest du wählen?");
 
   }
 }
