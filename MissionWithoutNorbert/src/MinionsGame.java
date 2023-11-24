a3471ee64db6e6d8f61c59e68e1f317f541ed0d1
Iris Olfermann <iris.olfermann@aoe.com>
2023-11-24 13:37:41 +0100

    lineUpMinions

--- MissionWithoutNorbert/src/MinionsGame.java
+++ MissionWithoutNorbert/src/MinionsGame.java
@@ -14,22 +14,61 @@ public class MinionsGame {
     final int MAX_DRAW = 3;
 
     // Variablen um die Position zu bestimmen
-    int randomPosition = (int) (Math.random() * 10);
+    int randomPosition = drawRandomNumber(MINION);
     int leftSide = randomPosition;
     int rightSide = MINION - leftSide;
 
     // Variablen für die Teamgröße und Endausgabe?
-    int computerTeamSize;
-    int userTeamSize;
+    int computerTeamSize =0;
+    int userTeamSize=0;
+
 
     // Benutzer Optionen
     char drawSide;
     int drawRange;
-    
+
+    // Zufallszahl wer beginnt. 0 für Computer, 1 für Nutzer
+    int beginner = drawRandomNumber(1);
+    // o ist links, 1 ist rechts
+    int drawSideComputer;
 
     System.out.println("Mission without Nobert!");
+    lineupMinions(leftSide, rightSide, NORBERT);
+    if(beginner==0){
+      System.out.print("Der Computer zieht jetzt eine zufällige Anzahl von Minions.");
+      drawRange = drawRandomNumber(MAX_DRAW);
+      drawSideComputer = drawRandomNumber(1);
+      computerTeamSize += drawRange;
+      if (drawSideComputer==0) {
+        leftSide -= drawRange;
+      } else {
+        rightSide -= drawRange;
+      }
+    }
+    else{
+      System.out.println("Wählen Sie ihre stärkste Mannschaft!");
+      System.out.println("Tipp: Nobert (O) ist am schwächsten");
+      System.out.println("Es dürfen maximal 3 Minions pro Runde ziehen");
+      System.out.println("Du bist am zug. Von welcher Seite möchtest du wählen?");
+      drawSide = StaticScanner.next().charAt(0);
+      System.out.println("Wie viele Minions möchten Sie ziehen?");
+      drawRange =StaticScanner.nextInt();
+      userTeamSize += drawRange;
+      if (drawSide=='l'||drawSide=='L') {
+        leftSide -= drawRange;
+      } else if (drawSide=='r'||drawSide=='R'){
+        rightSide -= drawRange;
+      }
+    }
+    lineupMinions(leftSide, rightSide, NORBERT);
+  }
 
-    // Minions + Nobert aufgestellt!
+  public static int drawRandomNumber(int b){
+    return (int) (Math.random()*(b+1));
+  }
+
+  public static void lineupMinions(int leftSide, int rightSide, char NORBERT){
+   // Minions + Nobert aufgestellt!
     for (int i=0; i<leftSide; i++){
       System.out.print('X'+" ");
     }
@@ -38,19 +77,7 @@ public class MinionsGame {
       System.out.print('X'+" ");
     }
     System.out.println();
-
-    System.out.println("Wählen Sie ihre stärkste Mannschaft!");
-    System.out.println("Tipp: Nobert (O) ist am schwächsten");
-    System.out.println("Es dürfen maximal 3 Minions pro Runde ziehen");
-    System.out.println("Du bist am zug. Von welcher Seite möchtest du wählen?");
-
-    drawSide = StaticScanner.next().charAt(0);
-    System.out.println("Wie viele Minions möchten Sie ziehen?");
-    drawRange =StaticScanner.nextInt(); 
-
     
-
-
-
   }
+
 }
