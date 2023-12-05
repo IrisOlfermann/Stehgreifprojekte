c28ccf6c82ca477cd402e78a9434d959c5a9bf7d
Iris Olfermann <iris.olfermann@aoe.com>
2023-12-05 16:51:05 +0100

    labyrinth zum einfüllen

--- Labyrinth/src/Labyrinth.java
+++ Labyrinth/src/Labyrinth.java
@@ -2,4 +2,10 @@ import java.util.Scanner;
 public class Labyrinth {
     // Für die Eingabe vom Nutzer
     Scanner StaticScanner = new Scanner(System.in);
+    public static char[][][] labyrinthSelection(){
+        char labyrinth1[][][]= new char[1][1][1];
+        char labyrinth2[][][]= new char[1][1][1];
+        char labyrinth3[][][]= new char[1][1][1];
+        return labyrinth1;
+    }
 }
