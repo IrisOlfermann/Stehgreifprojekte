08a8aa7c2a86b162b49095e7c5b23f876710d3e1
Dominik <dominik.kulak.15@gmail.com>
2023-12-12 13:16:22 +0100

    2 Labrinthe erstellt

--- MissionWithoutNorbert/src/LabyrinthGame.java
+++ MissionWithoutNorbert/src/LabyrinthGame.java
@@ -1,24 +1,73 @@
 import java.util.Scanner;
-
 public class LabyrinthGame {
-public static void main(String[] args) {
+    public static void main(String[] args){
+
+    // Für die Eingabe vom Nutzer
     Scanner StaticScanner = new Scanner(System.in);
-
-    int[][]Labyrinth=new int[][]{{1,1,1,1,1,0,1},
-                                 {1,0,0,0,1,0,1},
-                                 {1,0,1,0,1,0,1},
-                                 {1,0,1,0,0,0,1},
-                                 {1,0,1,1,1,1,1}};
-
-    for(int zeile=0; zeile<Labyrinth.length;zeile++){
-        for(int spalte=0; spalte<Labyrinth[0].length; spalte++){
-            System.out.print(Labyrinth[zeile][spalte]);
-            
-        }
-        System.out.println();
-        
     }
 
-}
+    public static char[][] labyrinthSelection(){
+        char[][]Labyrinth_1=new char[][]{{╔,═,═,═,═,═,═,═,╗}
+                                         {║,*,*,*,*,*,*,*,║},
+                                         {║,*,║,═,║,/,║,*,║},
+                                         {║,*,/,/,║,/,║,*,║},
+                                         {║,*,╔,/,║,/,║,*,A},
+                                         {╚,B,╩,=,╩,═,╩,═,╝}};
 
+        
+
+        char[][]Labyrinth_2=new char[][]{{╔,═,═,═,╦,═,═,═,═,═,╗}
+                                         {║,×,×,×,║,×,×,×,×,×,║},
+                                         {║,═,×,╔,╝,×,║,×,║,×,║},
+                                         {║,×,×,║,×,×,║,×,║,×,║},
+                                         {║,×,×,×,×,×,║,×,║,×,A},
+                                         {║,×,╔,×,║,×,║,×,×,×,║}
+                                         {║,×,║,×,╚,═,╣,×,║,×,║}
+                                         {║,×,║,×,×,×,║,×,║,×,║}
+                                         {B,×,╠,═,═,×,║,×,×,×,║}
+                                         {╚,═,╩,/,═,═,╩,═,═,═,╝}};
+        char labyrinth3[][]= new char[1][1];
+        int selectedLabyrinth;
+
+        System.out.println("Welches Labyrinth 1,2 oder 3 soll BB2 durchlaufen?");
+        // Fehlererkennung bei der Nutzereingabe
+        selectedLabyrinth = recognizeErrors();
+        // Zurückgabe des ausgewählten Labyrinths
+        switch (selectedLabyrinth) {
+            case 1:
+                return labyrinth1;
+            case 2:
+                return labyrinth2;
+            case 3:
+                return labyrinth3;
+                // nochmal angucken
+            default:
+            return labyrinth1;
+        }
+    }
+    public static int recognizeErrors(){
+        // auch hier muss ein StaticScanner Objekt erzeugt werden, um die Methode zu verwenden.
+          Scanner StaticScanner = new Scanner(System.in);
+          boolean validInput= false;
+          int drawNumber=0;
+          String drawRange = StaticScanner.nextLine();
+
+          while(validInput!= true){
+            // fängt ab, falls ein falscher Datentyp eingegeben wird.
+            try {
+                drawNumber = Integer.parseInt(drawRange);
+            } catch (NumberFormatException e) {
+                System.out.println("Die Eingabe ist keine Zahl. Bitte eine Zahl zwischen 1-3 eingeben.");
+            }
+            // Wenn klar ist, dass es eine Zahl ist, wird geprüft, ob sie zwischen 1-3 liegt.
+              if (drawNumber==1 || drawNumber ==2 || drawNumber==3) {
+                validInput = true;
+              }
+              else{
+                System.out.println("Ungültige Eingabe. Wähle eine Zahl zwischen 1-3.");
+                drawRange =StaticScanner.nextLine();
+              }
+          }
+       return drawNumber;
+      }
 }
